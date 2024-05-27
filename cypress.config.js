704e2069af0bbd67a922abe6e3aa95e494419513
ilyas <ilyas.soufiane96@gmail.com>
2024-05-27 11:04:07 +0200

    install Allure Report packages

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const { allureCypress } = require('allure-cypress/reporter');
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      allureCypress(on);
+    },
     // Configure your E2E tests here
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
   },

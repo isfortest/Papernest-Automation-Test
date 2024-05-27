a507ae2c4db603cbc84a0723a70a44b556ae3ba6
isfortest <isfortest@users.noreply.github.com>
2024-05-27 20:17:06 +0000

    deploy: a6c088cfc45fca65efad12b3cf576cee1a45c766

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
-  },
-  chromeWebSecurity: false
-})
